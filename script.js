589d8e0ef6ee212d19b6e00cfb3fc465e59408df
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-07 14:50:45 +0600

    added JavaScript

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let h1Element = document.createElement('h1');//create
 h1Element.textContent = "H1 header";//modify
 exercisesElement.append(h1Element);//attach
 
-
 let h2Element = document.createElement('h2');
 h2Element.textContent = "H2 header";
 exercisesElement.append(h2Element);
@@ -16,3 +15,21 @@ exercisesElement.append(h3Element);
 let h4Element = document.createElement('h4');
 exercisesElement.append(h4Element);
 h4Element.textContent = "H4 header";
+
+let h5Element = document.createElement('h5');
+exercisesElement.prepend(h5Element);
+h5Element.textContent = "H5 header";
+
+let h6Element = document.createElement('h6');
+exercisesElement.before(h6Element);
+h6Element.textContent = "H6 header";
+
+
+
+let buttonElement = document.createElement('button');
+buttonElement.textContent = "Launch Meeting";
+exercisesElement.append(buttonElement);
+buttonElement.style.backgroundColor = "blue";
+buttonElement.style.height = "40px";
+buttonElement.style.borderRadius = "5px";
+buttonElement.style.color = "white";
