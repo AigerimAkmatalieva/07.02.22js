b8c5ed06b95e46b1694a7b79511ad2fe33206113
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-07 14:18:52 +0600

    added JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+let exercisesElement = document.querySelector('#exercises');
+
+let h1 = document.createElement('h1');//create
+h1Element.textContent = "H1 header";//modify
+exercisesElement.append(h1Element);//attach
+
