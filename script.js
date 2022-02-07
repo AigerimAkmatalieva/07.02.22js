1eecfe096d8869eb471a56d4f41dda41f2923f7e
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-07 14:25:55 +0600

    added JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let exercisesElement = document.querySelector('#exercises');
 
-let h1 = document.createElement('h1');//create
+let h1Element = document.createElement('h1');//create
 h1Element.textContent = "H1 header";//modify
 exercisesElement.append(h1Element);//attach
 
+
+let h2Element = document.createElement('h2');
+h2Element.textContent = "H2 header";
+exercisesElement.append(h2Element);
+
+let h3Element = document.createElement('h3');
+h3Element.textContent = "H3 header";
+exercisesElement.append(h3Element);
